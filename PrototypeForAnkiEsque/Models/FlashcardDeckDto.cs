8d27c21eb6f13f01abf5f8801aff8fcdcf8b228e
Andrii Chemisov <andr.chemisov@gmail.com>
2025-02-24 14:01:18 +0200

    Added import/export functionality to flashcardDeckSelection. Adjusted FlashcardDeckDto to have EaseRating. It will be Hard by default, and will update if the user rates at least 1 flashcard.

--- PrototypeForAnkiEsque/Models/FlashcardDeckDto.cs
+++ PrototypeForAnkiEsque/Models/FlashcardDeckDto.cs
@@ -4,5 +4,6 @@ namespace PrototypeForAnkiEsque.Models
     {
         public string DeckName { get; set; }
         public List<FlashcardDto> Flashcards { get; set; }
+        public string EaseRating { get; set; } = "Hard";
     }
 }
